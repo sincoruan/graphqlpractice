d0d744837f809d75fa16913e7841806fce892155
xingkeruan <xingkeruan@Xingkes-MacBook-Air.local>
2023-12-24 11:50:20 -0800

    DataFetchingEnvironment 是 GraphQL Java 中的一个接口，它提供了在执行 GraphQL 查询时，用于获取有关当前字段、查询、上下文等信息的一种方式。通过这个接口，你可以访问有关查询执行环境的各种信息，以便在 Resolvers 中执行相应的数据获取逻辑。

--- src/main/java/com/sample/graphql/resolvers/mutation/BankAccountMutation.java
+++ src/main/java/com/sample/graphql/resolvers/mutation/BankAccountMutation.java
@@ -4,6 +4,7 @@ import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.sample.graphql.modal.BankAccount;
 import com.sample.graphql.modal.Currency;
 import com.sample.graphql.modal.input.CreateBankAccountInput;
+import graphql.schema.DataFetchingEnvironment;
 import org.intellij.lang.annotations.JdkConstants;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,8 @@ import java.util.UUID;
 
 @Component
 public class BankAccountMutation implements GraphQLMutationResolver {
-    public BankAccount createBankAccount(CreateBankAccountInput createBankAccountInput) {
+    public BankAccount createBankAccount(CreateBankAccountInput createBankAccountInput, DataFetchingEnvironment e) {
+
         return BankAccount.builder().id(UUID.randomUUID()).currency(Currency.CH).build();
     }
 }
